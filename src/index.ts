5992d1193af998435b7010cc3858571a33f775be
Eric L. Bentley <11730553+ebntly@users.noreply.github.com>
2019-02-24 14:57:10 -0600

    Add documentation

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,36 @@
 import { IncomingMessage } from "http";
 import { IResourceACLMiddleware } from "./types";
+export * from "./types";
 
+/**
+ * Middleware that determines whether a request from a domain is made.
+ */
 export const onDomain: IResourceACLMiddleware = {
+    /**
+     * @var string[] The description of the middleware.
+     */
     description: [
         "Prevent or allow access based on the domain a visitor is using.",
         "Can be used to prevent access from a particular web application or site.",
     ],
+    /**
+     * @var string The name of the middleware.
+     */
     display: "On Domain",
+    /**
+     * Accepts a request and a domain name, and returns whether the `host`
+     * property of the `request.headers` matches.
+     *
+     * @param req Partial<IncomingMessage>
+     * @param domain string
+     */
     exec(req: Partial<IncomingMessage>, domain: string) {
         return req.headers ? req.headers.host === domain : false;
     },
+    /**
+     * The collection of [[IResourceACLMiddlewareFormInput|input]] fields used
+     * to build the form when defining permissions.
+     */
     form: [
         {
             description: "Enter the domain name to allow/disallow requests from.",
@@ -21,6 +42,11 @@ export const onDomain: IResourceACLMiddleware = {
             validate: /^((?:(?:(?:\w[.\-+]?)*)\w)+)((?:(?:(?:\w[.\-+]?){0,62})\w)+)\.(\w{2,6})$/,
         },
     ],
+    /**
+     * An array of [[ResourceACLMiddlewareParamHandler|handlers]] that
+     * determine the parameters to store when processing the form data. This
+     * information is what is passed to the [[exec()]] function.
+     */
     handleParams: [
         (formValues: Array<string | number | boolean>) => {
             return formValues[0];
